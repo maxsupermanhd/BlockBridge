772805624658bf9e2e2ebadd48774fd44d98085e
maxsupermanhd <q3.max.2011@ya.ru>
2022-05-05 10:01:53 +0300

    mc to bot

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"os/signal"
-	"syscall"
 
+	"github.com/Tnze/go-mc/bot"
+	"github.com/Tnze/go-mc/bot/basic"
 	"github.com/Tnze/go-mc/chat"
 	"github.com/bwmarrin/discordgo"
+	"github.com/google/uuid"
 	"github.com/maxsupermanhd/WebChunk/credentials"
 	"github.com/natefinch/lumberjack"
 )
@@ -56,16 +58,11 @@ func main() {
 		Compress: true,
 	}))
 	log.Println("Hello world")
-	credentials.NewMicrosoftCredentialsManager(loadedConfig.CredentialsRoot, "88650e7e-efee-4857-b9a9-cf580a00ef43")
 	log.Print("Connecting to Discord...")
 	dg, err := discordgo.New("Bot " + loadedConfig.DiscordToken)
-	if err != nil {
-		log.Println("error creating Discord session,", err)
-		return
-	}
+	must(err)
 	dtom := make(chan string, 64)
 	defer close(dtom)
-	// mtod := make(chan string, 64)
 	dg.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		if m.Author.Bot || m.ChannelID != loadedConfig.ChannelID {
 			return
@@ -75,14 +72,71 @@ func main() {
 	})
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
 	err = dg.Open()
-	if err != nil {
-		log.Println("error opening connection,", err)
-		return
-	}
+	must(err)
 	defer dg.Close()
+
+	mtod := make(chan string, 64)
+	go func() {
+		for msg := range mtod {
+			log.Printf("m->d [%v]", msg)
+			dg.ChannelMessageSend(loadedConfig.ChannelID, msg)
+		}
+	}()
 	log.Print("Connected to Discord.")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-	log.Println("Roger, stopping shit.")
+	log.Print("Preparing credentials...")
+
+	client := bot.NewClient()
+	credman := credentials.NewMicrosoftCredentialsManager(loadedConfig.CredentialsRoot, "88650e7e-efee-4857-b9a9-cf580a00ef43")
+	botauth, err := credman.GetAuthForUsername(loadedConfig.MCUsername)
+	must(err)
+	if botauth == nil {
+		log.Fatal("botauth nil")
+	}
+	client.Auth = *botauth
+	basic.EventsListener{
+		GameStart: func() error {
+			mtod <- "Logged in"
+			return nil
+		},
+		ChatMsg: func(c chat.Message, pos byte, uuid uuid.UUID) error {
+			msg := c.Text
+			for _, em := range c.Extra {
+				msg += em.Text
+			}
+			mtod <- msg
+			return nil
+		},
+		Disconnect: func(reason chat.Message) error {
+			return DisconnectErr{Reason: reason}
+		},
+		HealthChange: nil,
+		Death: func() error {
+			mtod <- "Died lmao"
+			return nil
+		},
+	}.Attach(client)
+	must(client.JoinServer(loadedConfig.ServerAddress))
+	for {
+		if err = client.HandleGame(); err == nil {
+			panic("HandleGame never return nil")
+		}
+		if err2 := new(bot.PacketHandlerError); errors.As(err, err2) {
+			if err := new(DisconnectErr); errors.As(err2, err) {
+				log.Print("Disconnect: ", err.Reason)
+				return
+			} else {
+				log.Print(err2)
+			}
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
+
+type DisconnectErr struct {
+	Reason chat.Message
+}
+
+func (d DisconnectErr) Error() string {
+	return "disconnect: " + d.Reason.String()
 }
