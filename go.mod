9af0a3b597f765af7b24e8839a58f326fb68ddc4
maxsupermanhd <q3.max.2011@ya.ru>
2022-05-05 09:31:21 +0300

    Hello discord

--- go.mod
+++ go.mod
@@ -2,8 +2,18 @@ module github.com/maxsupermanhd/BlockBridge
 
 go 1.18
 
+require github.com/maxsupermanhd/WebChunk v0.0.0-20220504192725-94ac5dfeb7a7
+
 require (
-	github.com/Tnze/go-mc v1.18.1 // indirect
-	github.com/google/uuid v1.1.1 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
+)
+
+require (
+	github.com/Tnze/go-mc v1.18.2-0.20220401165811-e00bb53f9efb // indirect
+	github.com/bwmarrin/discordgo v0.25.0
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/maxsupermanhd/go-mc-ms-auth v0.0.0-20220223195356-5256511fc797 // indirect
 	github.com/natefinch/lumberjack v2.0.0+incompatible // indirect
 )
--- main.go
+++ main.go
@@ -7,17 +7,21 @@ import (
 	"os"
 
 	"github.com/Tnze/go-mc/chat"
+	"github.com/bwmarrin/discordgo"
+	"github.com/maxsupermanhd/WebChunk/credentials"
 	"github.com/natefinch/lumberjack"
 )
 
 type botConf struct {
-	ServerAddress string
-	DiscordToken  string
-	AllowedSlash  []string
-	SpamInterval  int
-	SpamMessage   chat.Message
-	LogsFilename  string
-	LogsMaxSize   int
+	ServerAddress   string
+	DiscordToken    string
+	AllowedSlash    []string
+	SpamInterval    int
+	SpamMessage     chat.Message
+	LogsFilename    string
+	LogsMaxSize     int
+	CredentialsRoot string
+	MCUsername      string
 }
 
 var loadedConfig botConf
@@ -48,4 +52,24 @@ func main() {
 		Compress: true,
 	}))
 	log.Println("Hello world")
+	credentials.NewMicrosoftCredentialsManager(loadedConfig.CredentialsRoot, "88650e7e-efee-4857-b9a9-cf580a00ef43")
+	log.Print("Connecting to Discord...")
+	dg, err := discordgo.New("Bot " + loadedConfig.DiscordToken)
+	if err != nil {
+		log.Println("error creating Discord session,", err)
+		return
+	}
+	dg.AddHandler(messageCreate)
+	dg.Identify.Intents = discordgo.IntentsGuildMessages
+	err = dg.Open()
+	if err != nil {
+		log.Println("error opening connection,", err)
+		return
+	}
+	defer dg.Close()
+	log.Print("Connected to Discord.")
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	log.Printf("Message from channel %v [%v]", m.ChannelID, m.Content)
 }
